293318bbb41efba34647498443bf6685a1758061
Goakem <195132463+Goakem@users.noreply.github.com.>
2026-06-11 09:41:50 -0400

    Pseudocode Update

---
+++ main.js
@@ -0,0 +1,13 @@
+// prompt user for login
+
+//enters admin prompt for password
+
+//If empty / escape show canceled
+
+// if another string show i don't know you
+
+// if equals the master show "Welcome!"
+
+// any other string show wrong password
+
+// for empty or canceled string show canceled
