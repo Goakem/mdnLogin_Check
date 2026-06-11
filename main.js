d9f5bbf849ac8cb1adfba086c6e72d1e247f028b
Goakem <195132463+Goakem@users.noreply.github.com.>
2026-06-11 10:30:07 -0400

    main code update

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 // prompt user for login
+let loginRequest = prompt("Who is there?");
 
 //enters admin prompt for password
+if(loginRequest === "Admin".toUpperCase()) {
+    let password = prompt("password?");
 
-//If empty / escape show canceled
+    // if equals the master show "Welcome!"
+    if (password === "Themaster") {
+        alert("welcome!");
+    
+    // if canceled or esc return canceled
+    }elseif(password === null) {
+        alert("canceled");
+    }else {
+        alert("wrong password");
+    }
 
+//If canceled / escape show canceled
+}elseif(loginRequest === null){
+    alert("canceled");  
+    
 // if another string show i don't know you
+}else{ 
+    alert ("I don't know you")
+}
 
-// if equals the master show "Welcome!"
-
-// any other string show wrong password
-
-// for empty or canceled string show canceled
